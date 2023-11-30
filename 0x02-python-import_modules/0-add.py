69526389bb801ae3ef268f6436a72faaba38b395
“Jeke01” <josephikoheke1@gmail.com>
2023-11-30 11:01:34 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
-a = 1
-b = 2
+
+"""
+Program to import the add function from add_0.py and perform addition.
+"""
+
+if __name__ == "__main__":
+    # Define variables a and b
+    a = 1
+    b = 2
+
+    # Import the add function from add_0.py
     from add_0 import add
+
+    # Calculate the result using the add function
     result = add(a, b)
-    print(f"{a} + {b} = {result}")
+
+    # Print the result using string formatting
+     print(f"{a} + {b} = {result}")
